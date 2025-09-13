1702241eb373fd8474f628cf0f101b5df0233946
yatajwale7777 <yatajwale7777@gmail.com>
2025-09-13 20:24:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // ====== Configuration: set your Apps Script URL here ======
-window.APPSCRIPT_URL = "https://script.google.com/macros/s/AKfycbypytJAcMHYmmRnx-OnboVtq0oK-LLEcFMZC54LjSUoV_kolD5plPyGcSuCNlkdHmqHcw/exec";
+window.APPSCRIPT_URL = "https://script.google.com/macros/s/AKfycbyhBUpyUQh-zx-RtcayzfiYva2Z8NAu74PHf168FnpDnxsOyH0vWQCWA3oGc6r32Pw6qQ/exec";
 // ===========================================================
 
 /* helpers */
