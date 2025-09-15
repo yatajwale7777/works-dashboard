b105a4b752931db824119d0e78d2fc926de37e15
yatajwale7777 <yatajwale7777@gmail.com>
2025-09-15 20:14:56 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE = 'works-v1';
+self.addEventListener('install', ev => {
+  ev.waitUntil(caches.open(CACHE).then(c=> c.addAll(['/','/index.html','/app.js','/styles.css'])));
+});
+self.addEventListener('fetch', ev => {
+  ev.respondWith(caches.match(ev.request).then(r=> r || fetch(ev.request)));
+});
